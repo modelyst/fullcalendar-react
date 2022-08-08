bf109bff96d270aa24224c0f8b3052a786753e9c
Michael Statt <michael.statt@modelyst.io>
2022-08-07 23:43:27 -0400

    fix: Modify React import to fix vite compatability issue

--- src/vdom.ts
+++ src/vdom.ts
@@ -1,54 +1,56 @@
-import * as react from 'react'
-import * as reactDom from 'react-dom'
+import React from "react";
+import * as reactDom from "react-dom";
 
-import ReactJSX = JSX // our reference to the JSX namespace
+import ReactJSX = JSX; // our reference to the JSX namespace
 
-export type ReactComponentChild = react.ReactNode
+export type ReactComponentChild =
+  | React.ReactNode
   | object
   | string
   | number
   | boolean
   | null
-  | undefined
+  | undefined;
 
 declare global {
   namespace FullCalendarVDom {
-    export import Ref = react.Ref
-    export import RefObject = react.RefObject
-    export import ComponentType = react.ComponentType
-    export import VNode = react.ReactNode
-    export import Context = react.Context
-    export import Component = react.Component
-    export type ComponentChild = ReactComponentChild
-    export type ComponentChildren = ReactComponentChild | ReactComponentChild[]
-    export import createElement = react.createElement
-    export import render = reactDom.render
-    export import createRef = react.createRef
-    export import Fragment = react.Fragment
-    export import createContext = react.createContext
-    export import createPortal = reactDom.createPortal
-    export type VUIEvent = react.UIEvent
-    export function flushSync(callback: () => void): void
-    export function unmountComponentAtNode(node: HTMLElement): void
+    export import Ref = React.Ref;
+    export import RefObject = React.RefObject;
+    export import ComponentType = React.ComponentType;
+    export import VNode = React.ReactNode;
+    export import Context = React.Context;
+    export import Component = React.Component;
+    export type ComponentChild = ReactComponentChild;
+    export type ComponentChildren = ReactComponentChild | ReactComponentChild[];
+    export import createElement = React.createElement;
+    export import render = reactDom.render;
+    export import createRef = React.createRef;
+    export import Fragment = React.Fragment;
+    export import createContext = React.createContext;
+    export import createPortal = reactDom.createPortal;
+    export type VUIEvent = React.UIEvent;
+    export function flushSync(callback: () => void): void;
+    export function unmountComponentAtNode(node: HTMLElement): void;
   }
   namespace createElement {
-    export import JSX = ReactJSX // preact exports the h.JSX namespace whereas react has it global. use preact's technique
+    export import JSX = ReactJSX; // preact exports the h.JSX namespace whereas react has it global. use preact's technique
   }
 }
 
-(typeof globalThis !== 'undefined' ? globalThis : window).FullCalendarVDom = { // TODO: streamline when killing IE11 support
-  Component: react.Component,
-  createElement: react.createElement,
+(typeof globalThis !== "undefined" ? globalThis : window).FullCalendarVDom = {
+  // TODO: streamline when killing IE11 support
+  Component: React.Component,
+  createElement: React.createElement,
   render: reactDom.render, // never called by FullCalendar's React component
-  createRef: react.createRef,
-  Fragment: react.Fragment,
-  createContext: react.createContext,
+  createRef: React.createRef,
+  Fragment: React.Fragment,
+  createContext: React.createContext,
   createPortal: reactDom.createPortal,
   flushSync,
-  unmountComponentAtNode: reactDom.unmountComponentAtNode // never called by FullCalendar's React component
-}
+  unmountComponentAtNode: reactDom.unmountComponentAtNode, // never called by FullCalendar's React component
+};
 
 export function flushSync(callback) {
   // always sync from top-level
-  callback()
+  callback();
 }
